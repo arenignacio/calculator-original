9ffac3fced497c2cbe31e33769d0536980972ce1
emignacio <edsiaren.ignacio@gmail.com>
2020-09-13 15:55:36 -0400

    initial commit

---
+++ newProject.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm init -y
+npm install webpack --save-dev
+npm install webpack-cli --save-dev
+npm install webpack-dev-server --save-dev
+npm install lodash
+npm install normalize.css
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const postCSSPlugins = [
+	require('postcss-import'),
+	require('postcss-mixins'),
+	require('postcss-simple-vars'),
+	require('postcss-nested'),
+	require('autoprefixer'),
+];
+
+module.exports = {
+	entry: './app/assets/scripts/App.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'app'), //#__dirname resolves to path of file being executed, ./ resolves to current location of executor (run "node Sandbox/node\ experiment/test.js" for example)
+	},
+	devServer: {
+		//holds files in ram. making delivery faster. installed using npm install webpack-dev-server --save-dev
+		before: function (app, server) {
+			//allow live loading of html
+			server._watch('./app/**/*.html');
+		},
+		contentBase: path.join(__dirname, 'app'),
+		hot: true, //allows browser to talk with files. if files change, browser automatically refreshes.
+		port: 3000,
+		host: '0.0.0.0', //host allows other devices in network to view live page. connect using (ipaddress):3000
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/i, //test for files with .css extension
+				use: [
+					//see note at closing bracket
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: postCSSPlugins, //postcss-loader needs plugins to work. options is evaluated right to left
+						},
+					},
+				], //use style-loader(applies), css-loader(loads), and postcss-loader(scss) when test result is true
+			},
+		],
+	},
+};
