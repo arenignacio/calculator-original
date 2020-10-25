68c140ef4631f5f5a9edc31b069c818ca4f01b4a
Aren Ignacio <business.arenignacio@gmail.com>
2020-10-25 15:50:22 -0400

    remove unnecessary files in repo

--- newProject.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npm init -y
-npm install webpack --save-dev
-npm install webpack-cli --save-dev
-npm install webpack-dev-server --save-dev
-npm install lodash
-npm install normalize.css
